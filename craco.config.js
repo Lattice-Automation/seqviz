668007bbdd5c273884ea26324995f0655c5fea75
Raymond Chung <rchung@latticeautomation.com>
2019-06-25 16:40:26 -0400

    feature: Add prettier UI with Semantic-UI-React

---
+++ craco.config.js
@@ -0,0 +1,43 @@
+const {
+  getLoader,
+  loaderByName,
+  throwUnexpectedConfigError
+} = require("@craco/craco");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "../../theme.config$": require("path").join(
+        __dirname,
+        "/src/semantic-ui/theme.config"
+      )
+    }
+  },
+  plugins: [
+    { plugin: require("craco-less") },
+    {
+      plugin: {
+        overrideWebpackConfig: ({ context, webpackConfig }) => {
+          const { isFound, match: fileLoaderMatch } = getLoader(
+            webpackConfig,
+            loaderByName("file-loader")
+          );
+
+          if (!isFound) {
+            throwUnexpectedConfigError({
+              message: `Can't find file-loader in the ${
+                context.env
+              } webpack config!`
+            });
+          }
+
+          fileLoaderMatch.loader.exclude.push(/theme.config$/);
+          fileLoaderMatch.loader.exclude.push(/\.variables$/);
+          fileLoaderMatch.loader.exclude.push(/\.overrides$/);
+
+          return webpackConfig;
+        }
+      }
+    }
+  ]
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,398 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Button,
+  Input,
+  Icon,
+  Menu,
+  Container,
+  Sidebar,
+  Dropdown,
+  Checkbox
+} from "semantic-ui-react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const backBoneOptions = [
+  { key: "psb1c3", value: "psb1c3", text: "pSB1C3" },
+  { key: "bba_k1362091", value: "bba_k1362091", text: "BBa_K1362091" },
+  { key: "bba_k823055", value: "bba_k823055", text: "BBa_K823055" },
+  { key: "psb1a3", value: "psb1a3", text: "pSB1A3" },
+  { key: "psb1a7", value: "psb1a7", text: "pSB1A7" },
+  { key: "psb1ac3", value: "psb1ac3", text: "pSB1AC3" },
+  { key: "psb1ak3", value: "psb1ak3", text: "pSB1AK3" },
+  { key: "psb1at3", value: "psb1at3", text: "pSB1AT3" },
+  { key: "psb1k3", value: "psb1k3", text: "pSB1K3" },
+  { key: "psb1t3", value: "psb1t3", text: "pSB1T3" }
+];
+export class BackBoneInput extends Component {
+  state = { value: "" };
+  render() {
+    const { setDemoState } = this.props;
+    return (
+      <span>
+        <Dropdown
+          trigger={<Icon name="circle notched" />}
+          button
+          className="icon"
+          options={backBoneOptions}
+          placeholder="Select a plasmid backbone"
+          onChange={(event, data) => {
+            this.setState({ value: data.value });
+            setDemoState({ backbone: data.value });
+          }}
+        />
+        {this.state.value}
+      </span>
+    );
+  }
 }
 
-export default App;
+const viewerTypeOptions = [
+  { key: "both", value: "both", text: "Both" },
+  { key: "circular", value: "circular", text: "Circular" },
+  { key: "linear", value: "linear", text: "Linear" }
+];
+
+export class ViewerTypeInput extends Component {
+  render() {
+    const { setDemoState } = this.props;
+    return (
+      <div>
+        Viewer Type:
+        <br />
+        <br />
+        <Dropdown
+          defaultValue="both"
+          fluid
+          selection
+          options={viewerTypeOptions}
+          onChange={(event, data) => {
+            setDemoState({ viewType: data.value });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export class LinearZoomInput extends Component {
+  render() {
+    const { setDemoState } = this.props;
+    return (
+      <div>
+        Zoom Linear:
+        <br />
+        <br />
+        <input
+          type="range"
+          min="1"
+          max="100"
+          defaultValue="50"
+          onChange={e => {
+            setDemoState({ lzoom: e.target.value });
+          }}
+          className="slider"
+          id="lzoom"
+        />
+      </div>
+    );
+  }
+}
+
+export class SearchQueryInput extends Component {
+  render() {
+    const {
+      setDemoState,
+      searchResults: { searchResults = [] }
+    } = this.props;
+    return (
+      <div>
+        <Input
+          fluid
+          icon="search"
+          placeholder="Search..."
+          onChange={(event, data) => {
+            setDemoState({ query: data.value });
+          }}
+        />
+        <br />
+        {searchResults.length} results
+      </div>
+    );
+  }
+}
+
+const enzymeOptions = [
+  { key: "psti", value: "PstI", text: "PstI" },
+  { key: "ecori", value: "EcoRI", text: "EcoRI" },
+  { key: "xbai", value: "XbaI", text: "XbaI" },
+  { key: "spei", value: "SpeI", text: "SpeI" }
+];
+
+export class EnzymeInput extends Component {
+  render() {
+    const { setDemoState } = this.props;
+    return (
+      <div>
+        Enzymes:
+        <Dropdown
+          placeholder="Select enzymes"
+          fluid
+          multiple
+          selection
+          options={enzymeOptions}
+          onChange={(event, data) => {
+            setDemoState({ enzymes: data.value });
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export class CheckboxInput extends Component {
+  render() {
+    const { name, label, setDemoState } = this.props;
+    return (
+      <Checkbox
+        toggle
+        defaultChecked
+        name={name}
+        label={label}
+        onChange={(event, data) => {
+          setDemoState({ [name]: data.checked });
+        }}
+      />
+    );
+  }
+}
+
+export class PartInput extends Component {
+  render() {
+    const { setDemoState } = this.props;
+    return (
+      <Input
+        icon="search"
+        name="accession"
+        placeholder="Find a BioBrick ..."
+        onChange={(event, data) => {
+          setDemoState({ part: data.value });
+        }}
+      />
+    );
+  }
+}
+
+export class SequenceViewer extends Component {
+  render() {
+    const {
+      part = "",
+      backbone = "",
+      viewType: view = "both",
+      annotate = true,
+      annotations = true,
+      complement = true,
+      index = true,
+      query = "",
+      enzymes = [],
+      lzoom = 50,
+      setDemoState
+    } = this.props;
+    const viewType = view || "both";
+    const lattice = window.lattice;
+    const viewer = lattice.Viewer("demo-root", {
+      part: part,
+      backbone: backbone,
+      viewer: viewType,
+      annotate: annotate,
+      showAnnotations: annotations,
+      showComplement: complement,
+      showIndex: index,
+      zoom: { linear: lzoom },
+      onSelection: selection => {
+        setDemoState({ selection: selection });
+      },
+      onSearch: results => {
+        setDemoState({ searchResults: results });
+      },
+      searchQuery: { query: query },
+      enzymes: Object.values(enzymes)
+    });
+    return part && viewer.viewer;
+  }
+}
+
+export class OptionsButton extends Component {
+  render() {
+    const { part, backbone, handleClick } = this.props;
+    return part && backbone ? (
+      <Button onClick={handleClick}>
+        <Icon name="bars" />
+        <label>Options</label>
+      </Button>
+    ) : (
+      <Button disabled onClick={handleClick}>
+        <Icon name="bars" />
+        <label>Options</label>
+      </Button>
+    );
+  }
+}
+
+export class SelectionInfo extends Component {
+  render() {
+    const { selection } = this.props;
+    const { feature, selectionMeta, sequenceMeta } = selection;
+
+    return (
+      selection && (
+        <div>
+          <div id="selection-name">{feature ? feature.name : ""}</div>
+          {selectionMeta && (
+            <div id="selection-meta">
+              {feature && feature.type && (
+                <span id="selection-type">{feature.type}</span>
+              )}
+              <span id="selection-length">
+                {selectionMeta.selectionLength}bp
+              </span>
+              <span id="selection-range">
+                ({selectionMeta.start} -{selectionMeta.end})
+              </span>
+            </div>
+          )}
+          {sequenceMeta && (
+            <div id="sequence-meta">
+              <span id="selection-label">GC:</span>
+              <span id="sequence-gc">{sequenceMeta.GC.toPrecision(2)}%</span>
+              <span id="selection-label">Tm:</span>
+              <span id="sequence-tm">{sequenceMeta.Tm.toPrecision(2)}°C</span>
+            </div>
+          )}
+        </div>
+      )
+    );
+  }
+}
+export class SideBarMenu extends Component {
+  state = { visible: false };
+
+  handleClick = () => {
+    const { visible } = this.state;
+    this.setState({ visible: !visible });
+  };
+  handleHide = () => {
+    this.setState({ visible: false });
+  };
+
+  render() {
+    const { visible } = this.state;
+    const { setDemoState } = this.props;
+    return (
+      <div style={{ height: "100vh" }}>
+        <Sidebar.Pushable stylename="sidebar-container">
+          <Sidebar
+            stylename="sidebar-container"
+            as={Menu}
+            animation="overlay"
+            vertical
+            onHide={this.handleHide}
+            visible={visible}
+          >
+            <OptionsButton {...this.props} handleClick={this.handleClick} />
+
+            <Menu.Item as="a">
+              <ViewerTypeInput setDemoState={setDemoState} />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <LinearZoomInput setDemoState={setDemoState} />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <SearchQueryInput
+                setDemoState={setDemoState}
+                searchResults={this.props.searchResults}
+              />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <CheckboxInput
+                setDemoState={setDemoState}
+                name="annotate"
+                label="Auto-annotate"
+              />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <CheckboxInput
+                setDemoState={setDemoState}
+                name="annotations"
+                label="Show annotations"
+              />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <CheckboxInput
+                setDemoState={setDemoState}
+                name="complement"
+                label="Show complement"
+              />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <CheckboxInput
+                setDemoState={setDemoState}
+                name="index"
+                label="Show axis"
+              />
+            </Menu.Item>
+            <Menu.Item as="a">
+              <EnzymeInput setDemoState={setDemoState} />
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher as={Container} fluid dimmed={visible}>
+            <div id="part-explorer">
+              <div id="header">
+                <OptionsButton {...this.props} handleClick={this.handleClick} />
+                <PartInput setDemoState={setDemoState} />
+                <BackBoneInput setDemoState={setDemoState} />
+              </div>
+              <div id="seqviewer">
+                <SequenceViewer {...this.props} />
+              </div>
+              <div id="footer">
+                <SelectionInfo {...this.props} />
+              </div>
+            </div>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
+  }
+}
+
+export class Demo extends Component {
+  state = {
+    part: "",
+    backbone: "",
+    viewType: "",
+    annotate: true,
+    annotations: true,
+    complement: true,
+    index: true,
+    query: "",
+    enzymes: [],
+    lzoom: 50,
+    selection: {},
+    searchResults: {}
+  };
+  setDemoState = state => {
+    let newState = Object.keys(state).reduce((newState, key) => {
+      if (typeof state[key] === "object") {
+        newState[key] = { ...this.state[key], ...state[key] };
+      } else {
+        newState[key] = state[key];
+      }
+      return newState;
+    }, {});
+    const { ...rest } = this.state;
+    this.setState({ ...rest, ...newState });
+  };
+  render() {
+    return <SideBarMenu {...this.state} setDemoState={this.setDemoState} />;
+  }
+}
