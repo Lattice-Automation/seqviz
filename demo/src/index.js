6b0b4947444093342b242572feef478a81a7c7b9
JJTimmons <joshua.timmons1@gmail.com>
2019-11-07 14:32:36 -0500

    add translations to README

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,10 +1,10 @@
-import * as serviceWorker from "./serviceWorker";
-import "semantic-ui-less/semantic.less";
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import "semantic-ui-less/semantic.less";
 
+import * as serviceWorker from "./serviceWorker";
 import { Demo } from "./App";
+import "./index.css";
 
 ReactDOM.render(<Demo />, document.getElementById("demo-root"));
 // If you want your app to work offline and load faster, you can change
