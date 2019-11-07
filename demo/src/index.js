ba4328a2b3146d9270f13f58e98481a03ae19387
JJTimmons <joshua.timmons1@gmail.com>
2019-11-07 15:17:45 -0500

    fix deploy script path
    - removing service workers
    - fixing deploy script

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-less/semantic.less";
 
-import * as serviceWorker from "./serviceWorker";
 import { Demo } from "./App";
 import "./index.css";
 
 ReactDOM.render(<Demo />, document.getElementById("demo-root"));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
