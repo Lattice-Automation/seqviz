72cd2062d74d452ae620b837472b6d31b2a3a133
JJTimmons <joshua.timmons1@gmail.com>
2019-12-05 13:15:24 -0500

    Add "home" button to demo seqviz logo
    - add click back to home to seqviz logo

--- src/io/externalToParts.js
+++ src/io/externalToParts.js
@@ -4,14 +4,14 @@ import { fetchBBB } from "./igemBackbones";
 /**
  * retrieve a string representation of a part from a remote server and convert it into a part
  */
-export default async (newPart, accession, options) => {
-  let igembrick = false;
+export default async (accession, options) => {
+  let igem = false;
   const { colors = [], backbone = "" } = options;
   // right now, we support either NCBI or iGEM. We parse this automatically. the user
   // doesn't specify the target registry, so we have to infer it from the passed accession
   let url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${accession.trim()}&rettype=gbwithparts&retmode=text`;
-  if (accession.includes("BB")) {
-    igembrick = true;
+  if (accession.startsWith("BB")) {
+    igem = true;
     // it's a BioBrick... target the iGEM repo
     url = `https://cors-anywhere.herokuapp.com/http://parts.igem.org/xml/part.${accession.trim()}`;
   } else {
@@ -28,24 +28,24 @@ export default async (newPart, accession, options) => {
       // make the call
       response = await fetch(url).then(response => response.text());
     } else {
-      const partRegistry = igembrick ? "iGEM" : "NCBI";
+      const partRegistry = igem ? "iGEM" : "NCBI";
 
       throw new Error(
-        `It looks like you are trying to fetch a part from ${partRegistry}, but could not connect to the registry. Please check that you have a stable network connection.`
+        `It looks like you are trying to fetch a part from ${partRegistry} but could not connect to the registry. Please check that you have a stable network connection.`
       );
     }
 
     // convert to a part
-    if (!igembrick && backbone.length) {
+    if (!igem && backbone.length) {
       console.warn(
         "The backbone option is currently only valid with BioBrick imports. If you meant to display a BioBrick, please make sure that your part input was a valid BioBrick accession number."
       );
     }
-    const igembackbone =
-      igembrick && backbone.length
+    const igemBackbone =
+      igem && backbone.length
         ? { name: backbone, backbone: fetchBBB(backbone) }
         : "";
-    if (igembrick && igembackbone === "") {
+    if (igem && igemBackbone === "") {
       console.warn(
         "It looks like you are trying to display a BioBrick. BioBricks typically need to be inserted into a Plasmid Backbone (https://parts.igem.org/Plasmid_backbones/Assembly). Please specify one in your viewer options."
       );
@@ -53,7 +53,7 @@ export default async (newPart, accession, options) => {
 
     const parts = await fileToParts(response, {
       colors: colors,
-      backbone: igembackbone
+      backbone: igemBackbone
     });
 
     if (parts && parts.length) return parts[0];
