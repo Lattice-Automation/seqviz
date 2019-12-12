d359dec767c48a6e2159e5efec098f4b5f7468eb
JJTimmons <joshua.timmons1@gmail.com>
2019-12-12 13:58:26 -0500

    Reduce bundle size, remove dependencies from bundle
    - remove babel-polyfill
    - remove all external dependencies from Webpack output in npm version
    - add a webpack config for the CDN that includes external dependencies
    - remove zip support (jslib was too large to warrant it)
    - fix snapgene.js so it uses ECMA modules/imports

--- jest.init.js
+++ jest.init.js
@@ -1,5 +1,6 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import sizeMe from "react-sizeme";
-import "babel-polyfill";
 
 var enzyme = require("enzyme");
 var Adapter = require("enzyme-adapter-react-16");
