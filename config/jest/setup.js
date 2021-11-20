e3f16d7b6223e36a5171a9b3f047463947610e0f
JJTimmons <joshua.timmons1@gmail.com>
2021-11-19 19:00:54 -0500

    Bump react peerDependency
    - to also allow ^17

--- config/jest/setup.js
+++ config/jest/setup.js
@@ -1,5 +1,5 @@
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import sizeMe from "react-sizeme";
 // import fetch from "jest-fetch-mock";
 import "core-js/stable";
