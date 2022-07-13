cc8de36ea048715bccda9ce5508f4508972b980a
jjtimmons <josh.timmons@hashicorp.com>
2022-07-12 20:35:00 -0500

    3.3.11

---
+++ config/scripts/release.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ "$(git rev-parse --abbrev-ref HEAD)" != "develop" ]; then
+  echo 'wrong branch: not on develop';
+  exit 1;
+fi
+
+npm run test:ci
+npm version "$1" --git-tag-version
+npm run build
+node ./markdown.js ../../README.md
+git add .
+git commit --amend -C HEAD
+npm publish
+
+version="$(npm pkg get version)"
+gh release create "$version" --title "$version" --generate-notes --target develop
