453b55df35adb3f63a3a33ff72811b6814741dd0
jjtimmons <josh.timmons@hashicorp.com>
2022-09-05 15:55:09 +0200

    Update demo assetPrefix

--- demo/next.config.js
+++ demo/next.config.js
@@ -1,3 +1,7 @@
-// const withLess = require("next-with-less");
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {};
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  // https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+  assetPrefix: isProd ? "https://tools.latticeautomation.com/seqviz" : undefined,
+};
