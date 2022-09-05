ce83539f70e579a478f031ec4280efcdd54408c7
jjtimmons <josh.timmons@hashicorp.com>
2022-09-05 16:19:57 +0200

    Add assetPrefix next hack

--- demo/next.config.js
+++ demo/next.config.js
@@ -4,4 +4,11 @@ module.exports = {
   // Use the CDN in production and localhost for development.
   // https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
   assetPrefix: isProd ? "https://tools.latticeautomation.com/seqviz" : undefined,
+  // https://github.com/vercel/next.js/issues/33488#issuecomment-1063677133
+  images: isProd
+    ? {
+        domains: ["https://tools.latticeautomation.com/seqviz"],
+        // path: "https://tools.latticeautomation.com/seqviz/_next/image",
+      }
+    : undefined,
 };
