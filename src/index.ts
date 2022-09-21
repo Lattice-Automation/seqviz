51e41da619dc845501305cdcb522d705361a84c1
Joshua Timmons <joshua.timmons1@gmail.com>
2022-09-21 12:47:35 +0200

    Import seqparse (#174)
    * Import seqparse
    
    * Remove url dependency
    
    * Fix demo, avoid iGEM
    
    * Bump setup-node
    
    * Remove npm i

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import * as ReactDOMServer from "react-dom/server";
+
+import SeqViz, { SeqVizProps } from "./SeqViz";
+import "./SeqViz.css";
+import enzymes from "./enzymes";
+
+/**
+ * Export a React component directly for React-based development
+ */
+export { SeqViz, SeqVizProps, enzymes as Enzymes };
+
+/**
+ * Return a Viewer object with three properties:
+ *  - `render` to an HTML element
+ *  - `setState(options)` to update the viewer's internal state
+ *  - `renderToString` to return an HTML representation of the Viewer
+ */
+const Viewer = (element: string | HTMLElement = "root", options: SeqVizProps) => {
+  // used to keep track of whether to re-render after a "set" call
+  let rendered = false;
+  // get the HTML element by ID or use as is if passed directly
+  let domElement: HTMLElement | null;
+  if (!document) return;
+
+  if (typeof element === "string") {
+    if (document.getElementById(element)) {
+      domElement = document.getElementById(element);
+    } else {
+      throw new Error(`Failed to find an element with ID: ${element}`);
+    }
+  } else {
+    domElement = element;
+  }
+  let viewer = React.createElement(SeqViz, options, null);
+
+  /**
+   * Render the Viewer to the element passed
+   */
+  const render = () => {
+    rendered = true;
+    ReactDOM.render(viewer, domElement);
+    return viewer;
+  };
+
+  /**
+   * Return an HTML string representation of the viewer
+   */
+  const renderToString = () => {
+    return ReactDOMServer.renderToString(viewer);
+  };
+
+  /**
+   * Update the viewer with new settings. Re-renders if render was already called.
+   */
+  const setState = (state: SeqVizProps) => {
+    options = { ...options, ...state };
+    viewer = React.createElement(SeqViz, options, null);
+
+    if (rendered) {
+      ReactDOM.render(viewer, domElement);
+    }
+    return viewer;
+  };
+
+  return {
+    render,
+    renderToString,
+    setState,
+  };
+};
+
+export { Viewer };
