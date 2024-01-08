61414e85ae68b6a4739da30de8554758b4122d15
Guzman <guzmanvigliecca@gmail.com>
2024-01-08 15:45:38 -0300

    Remove unused import

--- src/SeqViewerContainer.tsx
+++ src/SeqViewerContainer.tsx
@@ -6,7 +6,7 @@ import { EventHandler } from "./EventHandler";
 import Linear, { LinearProps } from "./Linear/Linear";
 import SelectionHandler, { InputRefFunc } from "./SelectionHandler";
 import CentralIndexContext from "./centralIndexContext";
-import { Annotation, CutSite, Highlight, NameRange, Primer, Range, SeqType } from "./elements";
+import { Annotation, CutSite, Highlight, NameRange, Primer, SeqType } from "./elements";
 import { isEqual } from "./isEqual";
 import SelectionContext, { Selection, defaultSelection } from "./selectionContext";
 
